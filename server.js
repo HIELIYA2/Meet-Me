9607cec8eb55c037e57820fdaa1494f1c8ebd503
Eliyahu Hillel <Hillel.eli396@gmail.com>
2020-03-09 19:43:36 +0200

    server move

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import { join } from "path";
 
 const app = express();
 
-//here we are configuring dist to serve app files
+// here we are configuring dist to serve app files
 app.use("/", serveStatic(join(__dirname, "/dist")));
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function(req, res) {
+app.get(/.*/, (req, res) => {
   res.sendFile(join(__dirname, "/dist/index.html"));
 });
 
